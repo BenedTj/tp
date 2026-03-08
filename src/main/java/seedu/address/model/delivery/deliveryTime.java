b586519360fb5626de4276fca83c3050abc4c3e5
BenedTj <benedicttjia.prof@gmail.com>
2026-03-08 15:24:18 +0800

    Change time to deliveryTime

---
+++ src/main/java/seedu/address/model/delivery/deliveryTime.java
@@ -0,0 +1,75 @@
+package seedu.address.model.delivery;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Delivery's time in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDeliveryTime(String)}
+ */
+public class deliveryTime {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Time should be of the valid delivery time format";
+
+    /**
+     * The time must follow the format HH:mm
+     * where HH is the hour value in the 24-hour format
+     * and mm is the minute value.
+     */
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+    public final LocalTime time;
+
+    /**
+     * Constructs a {@code Time}.
+     *
+     * @param time A valid time string in the valid format.
+     */
+    public deliveryTime(String time) {
+        requireNonNull(time);
+        checkArgument(isValidDeliveryTime(time), MESSAGE_CONSTRAINTS);
+        this.time = LocalTime.parse(time, formatter);
+    }
+
+    /**
+     * Returns true if a given string is a valid time
+     * in the valid format.
+     */
+    public static boolean isValidDeliveryTime(String test) {
+        try {
+            LocalTime.parse(test, formatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return time.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof deliveryTime)) {
+            return false;
+        }
+
+        deliveryTime otherTime = (deliveryTime) other;
+        return time.equals(otherTime.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return time.hashCode();
+    }
+}
